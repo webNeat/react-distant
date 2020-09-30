77761346d1859de99cadaa37b7eac2d8ddcd3e77
Amine Ben hammou <webneat@gmail.com>
2020-09-30 05:24:09 +0200

    Fix example

--- examples/todo-with-ssr/web/api.ts
+++ examples/todo-with-ssr/web/api.ts
@@ -1,24 +1,26 @@
 import axios from 'axios'
 import {Task} from '../types'
 
+const URL = 'http://localhost:3000/api'
+
 export async function all() {
-  const res = await axios.get(`/api/tasks`)
+  const res = await axios.get(`${URL}/tasks`)
   return res.data as Task[]
 }
 
 type AddFields = Omit<Task, 'id'>
 export async function add(fields: AddFields) {
-  const res = await axios.post('/api/tasks', fields)
+  const res = await axios.post(`${URL}/tasks`, fields)
   return res.data as Task
 }
 
 type EditFields = Partial<AddFields>
 export async function edit(id: number, fields: EditFields) {
-  const res = await axios.put(`/api/tasks/${id}`, fields)
+  const res = await axios.put(`${URL}/tasks/${id}`, fields)
   return res.data as Task
 }
 
 export async function remove(id: number) {
-  const res = await axios.delete(`/api/tasks/${id}`)
+  const res = await axios.delete(`${URL}/tasks/${id}`)
   return res.data
 }

fc04553733597d22091efe8feef43b72ca9ed990
Amine Ben hammou <webneat@gmail.com>
2020-12-17 20:02:25 +0100

    Release 1.0.0-beta.10

--- src/serverRender.ts
+++ src/serverRender.ts
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOMServer from 'react-dom/server'
 import {CacheProvider} from './CacheProvider'
 import {Cache} from './classes'
+import {wait} from './functions'
 
 export async function serverRender(element: React.ReactElement) {
   const cache = new Cache()
   let isLoading = true
   while (isLoading) {
     ReactDOMServer.renderToString(React.createElement(CacheProvider, {value: cache}, element))
+    await wait(10)
     isLoading = await cache.waitLoading()
   }
   const html = ReactDOMServer.renderToString(React.createElement(CacheProvider, {value: cache}, element))
